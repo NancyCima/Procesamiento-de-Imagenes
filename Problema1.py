903ca2d8dc63b35edea2882fd46e4cda59c01bfb
Nancy <nancy.cima.bertoni@gmail.com>
2024-10-23 11:17:04 -0300

    añadir filtrado

--- Problema1.py
+++ Problema1.py
@@ -96,3 +96,51 @@ plt.title(f'Histograma Ecualización (Ventana: {window_size_large})')
 
 plt.tight_layout()
 plt.show()
+
+#Decidimos que la imagen con ventana de tamaño 30x30 es la mejor opcion porque
+#puede revelar los detalles ocultos con detalle y no ser tan ruidosa.
+#Una opcion para eliminar el ruido agregado por la ecualizacion local del
+#histograma es aplicar un filtro. 
+
+def charmean(imgn, k=3, Q=1.5):
+    imgn_f = imgn.astype(np.float64)
+    if Q < 0:
+        imgn_f += np.finfo(float).eps
+    w = np.ones((k, k))
+    I_num = cv2.filter2D(imgn_f**(Q+1), cv2.CV_64F, w)
+    I_den = cv2.filter2D(imgn_f**Q, cv2.CV_64F, w)
+    I = I_num / (I_den + np.finfo(float).eps)
+    return I
+
+# Aplico el filtrado
+img_ch_filt = charmean(equalized_medium, 3, 1.5)
+img_median_filt = cv2.medianBlur(equalized_medium, 3)
+
+# Crear la figura para comparar las imágenes (1 fila, 3 columnas)
+fig, axs = plt.subplots(1, 3, figsize=(18, 6))
+
+# Mostrar la imagen ruidosa original
+axs[0].imshow(equalized_medium, cmap='gray')
+axs[0].set_title("Imagen ruidosa")
+axs[0].axis('off')
+
+# Mostrar la imagen filtrada con mediana
+axs[1].imshow(img_median_filt, cmap='gray')
+axs[1].set_title("Filtro mediana")
+axs[1].axis('off')
+
+# Mostrar la imagen filtrada con "Contrharmonic-Mean"
+axs[2].imshow(img_ch_filt, cmap='gray')
+axs[2].set_title("Filtro Contrharmonic-Mean")
+axs[2].axis('off')
+
+# Guardar la imagen comparativa
+plt.tight_layout()
+plt.savefig(r"Imagenes Obtenidas\Ecualizacion_local_30_sin_ruido.png")
+plt.show()
+
+#Aqui notamos que ambos filtros eliminan el ruido, que creemos que es de tipo pepper, pero el 
+# filtro Contrharmonic-Mean agrega ruido salt alrededor de los onjetos ocultos.
+# Por otro lado, el filtro mean no agrega mas ruido como tal pero redondea algunos de los
+# bordes de los objetos ocultos. 
+# Por lo tanto, el filtro mediana es la mejor opcion para eliminar el ruido en este caso.
